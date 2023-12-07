ff40b47788a909cfaca9ad33f4b704b57f783089
BaptisteC <baptiste.cayrier@gmail.com>
2023-12-07 16:34:21 +0100

    feat: add recipes type

---
+++ libs/shared/src/lib/recipes-dto.ts
@@ -0,0 +1,12 @@
+export type RecipeDto = {
+  id: string;
+  name: string;
+  description: string;
+  pictureUrl: string;
+  steps: string[];
+  ingredients: string[];
+};
+
+export type RecipesResponseDto = {
+  items: RecipeDto[];
+};
